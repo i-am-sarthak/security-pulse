4fd2cd54ce057cc9cbc5fc6b0861cff89efdcea4
i-am-sarthak <sarthaksharmav@gmail.com>
2025-11-14 01:02:10 +0530

    Enable SSl only in producion

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -5,9 +5,7 @@ dotenv.config();
 
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
     // user: process.env.DB_USER,
     // host: process.env.DB_HOST,
     // database: process.env.DB_NAME,
