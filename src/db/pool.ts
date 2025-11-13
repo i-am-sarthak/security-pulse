c9dfade17c47391b88241c936fe378e8faa8d956
i-am-sarthak <sarthaksharmav@gmail.com>
2025-11-14 00:33:43 +0530

    Enable SSL for Render PostgreSQL

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -5,6 +5,9 @@ dotenv.config();
 
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
     // user: process.env.DB_USER,
     // host: process.env.DB_HOST,
     // database: process.env.DB_NAME,
