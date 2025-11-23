473088d836cf0ad9f2ce8a297a9e0f9236153d1e
i-am-sarthak <sarthaksharmav@gmail.com>
2025-11-23 20:54:53 +0530

    Undo force IPv4

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,10 +1,7 @@
 import {Pool} from 'pg';
 import dotenv from 'dotenv';
-import dns from "dns";
 
 dotenv.config();
-
-dns.setDefaultResultOrder("ipv4first");
 
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
