fc6d3cc4e5be4e2af4364e37d9673477b06d7d24
i-am-sarthak <sarthaksharmav@gmail.com>
2025-11-23 20:27:49 +0530

    Force IPv4 for Supabase DB connection

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,7 +1,10 @@
 import {Pool} from 'pg';
 import dotenv from 'dotenv';
+import dns from "dns";
 
 dotenv.config();
+
+dns.setDefaultResultOrder("ipv4first");
 
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
